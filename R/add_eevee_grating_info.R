e024fad16054ba2117f66cc763e26410c9e5daf0
Brian Pile <brian.pile@gmail.com>
2026-03-17 09:15:43 +0800

    Add add_eevee_grating_info function. It is based on the GDS mapping of wafer R-POE-3I.

---
+++ R/add_eevee_grating_info.R
@@ -0,0 +1,38 @@
+add_eevee_grating_info = function(dat) {
+
+  dat |>
+    mutate(
+      grt_no =
+        dplyr::case_when(
+          cell_id == "23" ~
+            case_when(
+              .data$bar_id_decimal %in% (c(59, 65) + 0) ~ "1A",
+              .data$bar_id_decimal %in% (c(59, 65) + 1) ~ "1B",
+              .data$bar_id_decimal %in% (c(59, 65) + 2) ~ "2A",
+              .data$bar_id_decimal %in% (c(59, 65) + 3) ~ "2B",
+              .data$bar_id_decimal %in% (c(59, 65) + 4) ~ "3A",
+              .data$bar_id_decimal %in% (c(59, 65) + 5) ~ "3B",
+              .data$bar_id_decimal %in% (c(71, 73, 75, 77) + 0) ~ "4A",
+              .data$bar_id_decimal %in% (c(72, 74, 76, 78) + 0) ~ "4B",
+              .default = NA
+            ),
+
+          cell_id == "33" ~ NA,
+
+          .default =
+            case_when(
+              ((.data$bar_id_decimal - 1) %% CELL_NROW + 1) %in% (c(1, 7, 13) + 0) ~ "1A",
+              ((.data$bar_id_decimal - 1) %% CELL_NROW + 1) %in% (c(1, 7, 13) + 1) ~ "1B",
+              ((.data$bar_id_decimal - 1) %% CELL_NROW + 1) %in% (c(1, 7, 13) + 2) ~ "2A",
+              ((.data$bar_id_decimal - 1) %% CELL_NROW + 1) %in% (c(1, 7, 13) + 3) ~ "2B",
+              ((.data$bar_id_decimal - 1) %% CELL_NROW + 1) %in% (c(1, 7, 13) + 4) ~ "3A",
+              ((.data$bar_id_decimal - 1) %% CELL_NROW + 1) %in% (c(1, 7, 13) + 5) ~ "3B",
+              ((.data$bar_id_decimal - 1) %% CELL_NROW + 1) %in% (c(19, 21, 23, 25) + 0) ~ "4A",
+              ((.data$bar_id_decimal - 1) %% CELL_NROW + 1) %in% (c(19, 21, 23, 25) + 1) ~ "4B",
+
+              .default = NA
+            )
+
+        )
+    )
+}
--- dev/scratch1.R
+++ dev/scratch1.R
@@ -11,13 +11,16 @@ wafer_diameter_mm = 75
 circ_dat = generate_circle(diameter = wafer_diameter_mm * 1e3)
 
 # generate eevee die coords
-df_eevee_die_coords =
+df_eevee =
   generate_eevee_ids() |>
   add_eevee_die_coords() |>
-  mutate(r = sqrt(die_x_coord^2 + die_y_coord^2))
+  mutate(r = sqrt(die_x_coord^2 + die_y_coord^2)) |>
+  add_eevee_grating_info()
 
-# wafer map check ----
-df_eevee_die_coords |>
+# wafer map checks ----
+
+# cell ID
+df_eevee |>
   filter(r <= wafer_diameter_mm*1e3/2) |>
   ggplot(aes(xmin = die_x_coord, xmax = die_x_coord + DIE_WIDTH, ymin = die_y_coord, ymax = die_y_coord + DIE_LENGTH)) +
   geom_rect(aes(fill = as.factor(cell_id)), color = "white", linewidth = 0, na.rm = FALSE) +
@@ -28,16 +31,37 @@ df_eevee_die_coords |>
     color = "gray"
   ) +
   coord_equal(xlim = c(-80000/2, 80000/2)) +
-  # facet_wrap( ~ waferID) +
   # scale_fill_viridis_d(direction = 1, option = "plasma") +
-  scale_fill_viridis_d() +
+  # scale_fill_viridis_d() +
   theme(
     panel.grid = element_blank()
   ) +
   labs(
-    title = "EEVEE Chip Map",
+    title = "EEVEE Cell ID Map",
     fill = "Cell ID"
   ) +
   guides(fill=guide_legend(ncol=2))
 
+# grating no
+df_eevee |>
+  # filter(r <= wafer_diameter_mm*1e3/2) |>
+  ggplot(aes(xmin = die_x_coord, xmax = die_x_coord + DIE_WIDTH, ymin = die_y_coord, ymax = die_y_coord + DIE_LENGTH)) +
+  geom_rect(aes(fill = grt_no), color = "white", linewidth = 0, na.rm = FALSE) +
+  # geom_path(
+  #   data = circ_dat,
+  #   mapping = aes(x = x, y = y),
+  #   inherit.aes = FALSE,
+  #   color = "gray"
+  # ) +
+  coord_equal(xlim = c(-80000/2, 80000/2)) +
+  # scale_fill_viridis_d(direction = 1, option = "plasma") +
+  # scale_fill_viridis_d() +
+  theme(
+    panel.grid = element_blank()
+  ) +
+  labs(
+    title = "EEVEE Grating No. Map",
+    # fill = "Cell ID"
+  ) +
+  guides(fill=guide_legend(ncol=2))
 

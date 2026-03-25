fbd60103840fd498df97e89154eb339edb506cf6
Brian Pile <brian.pile@gmail.com>
2026-03-25 09:18:02 +0800

    update gitignore to excluse dev/ folder

--- dev/scratch1.R
+++ dev/scratch1.R
@@ -49,28 +49,19 @@ df_eevee |>
   filter(r <= wafer_diameter_mm*1e3/2) |>
   ggplot(aes(xmin = die_x_coord, xmax = die_x_coord + DIE_WIDTH, ymin = die_y_coord, ymax = die_y_coord + DIE_LENGTH)) +
   geom_rect(aes(fill = grt_no), color = "white", linewidth = 0, na.rm = FALSE) +
-  # geom_path(
-  #   data = circ_dat,
-  #   mapping = aes(x = x, y = y),
-  #   inherit.aes = FALSE,
-  #   color = "gray"
-  # ) +
   coord_equal(xlim = c(-80000/2, 80000/2)) +
-  # scale_fill_viridis_d(direction = 1, option = "plasma") +
-  # scale_fill_viridis_d() +
   theme(
     panel.grid = element_blank()
   ) +
   labs(
     title = "EEVEE Grating No. Map",
-    # fill = "Cell ID"
   ) +
   guides(fill=guide_legend(ncol=1))
 
 
 # grating design statistics ----
 df_eevee |>
-  drop_na(grt_no) |>
+  # drop_na(grt_no) |>
   filter(r <= wafer_diameter_mm*1e3/2) |>
   count(grt_no) |>
   mutate(
